0f90d0b988e27d031b7f4e948e9552c95e5bb3db
Martin Jediny <jedinym@proton.me>
2025-11-10 12:06:59 +0100

    docs(ISV-6227): update code documentation
    Signed-off-by: Martin Jediny <jedinym@proton.me>

--- pkg/containerfile/containerfile.go
+++ pkg/containerfile/containerfile.go
@@ -10,30 +10,49 @@ import (
 
 var FinalStage string = ""
 
+// A builder-type COPY command in a Containerfile stage.
+// A COPY is builder-type if it copies from a previous builder stage
+// or directly from an image.
 type Copy struct {
+	// Sources in the command.
 	Sources     []string
+	// Destination in the command.
 	Destination string
+	// Alias of the stage the command is copying from in the case
+	// of stage copies or a pullspec when copying directly from an image.
 	From        string
 }
 
+// A builder or final stage in a Containerfile
 type Stage struct {
+	// Alias of the builder stage or equal to FinalStage if final
 	Alias    string
+	// Base image for the stage
 	Pullspec string
+	// Builder copies in this stage
 	Copies   []Copy
 }
 
 type BuildOptions struct {
+	// Build arguments passed to buildah for the build
 	Args   map[string]string
+	// Target stage of the buildah build
 	Target string
 }
 
-// Parse takes the path to a dockerfile-json output file and
-// parses it into stages.
+// Parse reads a Containerfile from the passed reader and uses the passed
+// BuildOptions to parse the Containerfile into stages.
 func Parse(reader io.Reader, opts BuildOptions) (res []Stage, err error) {
 	node, err := imagebuilder.ParseDockerfile(reader)
 	if err != nil {
 		return res, err
 	}
+
+	// TODO: At this stage, Buildah code takes into account OS and ARCH CLI args
+	// and overrides the built-in TARGETOS and TARGETARCH args (and others).
+	// In Konflux build, this is currently not supported but I'm keeping this here
+	// as a guideline.
+	// https://github.com/containers/buildah/blob/main/imagebuildah/build.go#L431
 
 	builder := imagebuilder.NewBuilder(opts.Args)
 	rawStages, err := imagebuilder.NewStages(node, builder)
@@ -77,6 +96,8 @@ func argsMapToSlice(m map[string]string) []string {
 	return s
 }
 
+// mapAliasesToPullspecs uses the passed imagebuilder.Stage structs to create
+// a mapping between stage aliases and the base image pullspecs for those stages.
 func mapAliasesToPullspecs(stages []imagebuilder.Stage) (res map[string]string) {
 	res = make(map[string]string)
 
@@ -93,6 +114,12 @@ func mapAliasesToPullspecs(stages []imagebuilder.Stage) (res map[string]string) 
 	return res
 }
 
+// getBuilderCopiesInStage parses the AST for the passed imagebuilder.Stage and
+// returns a slice of Copy structs, specifying which builder-type copies are
+// present in that stage.
+// A COPY command is builder-type if the "--from" flag is specified and it copies from
+// a builder stage or directly from an image.
+// WARNING: named contexts in the Containerfile are not supported
 func getBuilderCopiesInStage(s imagebuilder.Stage) []Copy {
 	copies := make([]Copy, 0)
 	headingEnv := argsMapToSlice(s.Builder.HeadingArgs)
